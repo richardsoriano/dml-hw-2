ce4b05204611accc28c90b78ae6b1467b72ce1aa
Richard Soriano <59493601+richardsoriano@users.noreply.github.com>
2021-08-17 10:37:53 -0700

    initialize

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <form>
+        <div></div>
+        <div></div>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+
+        <div>
+          <hw>Contact Info</hw>
+          <div>
+            <div>
+              <div>
+                <label>Your name</label>:
+              </div>
+              <input type='text' id='fullname' placeholder='Jean Luc Picard' />
+            </div>
+
+            <div>
+              <div>
+                <label>Email</label>:
+              </div>
+              <input type='text' id='email' placeholder='abc@email.com' />
+            </div>
+
+            <div>
+              <div>
+                <label>Phone</label>:
+              </div>
+              <input type='text' id='phone' placeholder='222-222-2222' />
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <hw>Shipping Info</hw>
+          <div>
+            <div>
+              <div>
+                <label>Label</label>:
+              </div>
+              <input type='text' id='home' placeholder='Home' />
+            </div>
+
+            <div>
+              <div>
+                <label>Address</label>:
+              </div>
+              <input type='text' id='address' placeholder='8000 Sunset Blvd' />
+            </div>
+
+            <div>
+              <div>
+                <label>Unit</label>:
+              </div>
+              <input type='text' id='unit' placeholder='#21' />
+            </div>
+          </div>
+        </div>
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
